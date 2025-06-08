88f613f2cee30b77442cea9c36a9025f122aecec
baiano <baiano7458222@gmail.com>
2025-06-08 19:21:49 -0300

    arrumando os botões

--- pages/pagina_codigo_app.py
+++ pages/pagina_codigo_app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 
-st.set_page_config(layout="wide")  # Melhor para colunas
-
-# Página "Códigos"
+st.set_page_config(layout="wide")
 st.title("Página de Códigos")
+
+# Inicializa os "flags" de mostrar mais
+for i in range(1, 5):
+    if f"mostrar{i}" not in st.session_state:
+        st.session_state[f"mostrar{i}"] = False
 
 # Primeira linha de imagens
 col1, col2, col3 = st.columns(3)
@@ -14,8 +17,11 @@ with col2:
 with col3:
     st.image("image3.png")
 
-# Mostrar mais 1
+# Botão: Mostrar mais 1
 if st.button("Mostrar mais 1"):
+    st.session_state.mostrar1 = True
+
+if st.session_state.mostrar1:
     col4, col5, col6 = st.columns(3)
     with col4:
         st.image("image4.png")
@@ -24,33 +30,38 @@ if st.button("Mostrar mais 1"):
     with col6:
         st.image("image6.png")
 
-    # Mostrar mais 2 (dentro do 1)
     if st.button("Mostrar mais 2"):
-        col7, col8, col9 = st.columns(3)
-        with col7:
-            st.image("image7.png")
-        with col8:
-            st.image("image8.png")
-        with col9:
-            st.image("image9.png")
+        st.session_state.mostrar2 = True
 
-        # Mostrar mais 3 (dentro do 2)
-        if st.button("Mostrar mais 3"):
-            col10, col11, col12 = st.columns(3)
-            with col10:
-                st.image("image10.png")
-            with col11:
-                st.image("image11.png")
-            with col12:
-                st.image("image12.png")
+if st.session_state.mostrar2:
+    col7, col8, col9 = st.columns(3)
+    with col7:
+        st.image("image7.png")
+    with col8:
+        st.image("image8.png")
+    with col9:
+        st.image("image9.png")
 
-            # Mostrar mais 4 (último nível)
-            if st.button("Mostrar mais 4"):
-                col13, col14, col15 = st.columns(3)
-                with col13:
-                    st.image("image13.png")
-                with col14:
-                    st.image("image14.png")
-                with col15:
-                    st.image("image15.png")
+    if st.button("Mostrar mais 3"):
+        st.session_state.mostrar3 = True
 
+if st.session_state.mostrar3:
+    col10, col11, col12 = st.columns(3)
+    with col10:
+        st.image("image10.png")
+    with col11:
+        st.image("image11.png")
+    with col12:
+        st.image("image12.png")
+
+    if st.button("Mostrar mais 4"):
+        st.session_state.mostrar4 = True
+
+if st.session_state.mostrar4:
+    col13, col14, col15 = st.columns(3)
+    with col13:
+        st.image("image13.png")
+    with col14:
+        st.image("image14.png")
+    with col15:
+        st.image("image15.png")
